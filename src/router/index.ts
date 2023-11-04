431fc781ce30a52925b74d9791b6ab9592da7030
某某 <2480396479@qq.com>
2023-11-05 00:37:24 +0800

    router修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,59 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import type { RouteRecordRaw } from 'vue-router'
-import components from '../Layout/index.vue'
+// import type { RouteRecordRaw } from 'vue-router'
+// import components from '../Layout/index.vue'
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-Vue.use(VueRouter);
-export default new VueRouter({
-    routes:[
-        {
-            path: '/',
-            component:components,
-            redirect: '/',
-            children: [
-                {
-                    path: '/',
-                    name: 'recommend',
-                    component: () => import('../components/recommed.vue')
-                },
+import {Layout} from "ant-design-vue";
 
-                {
-                    path: '/user',
-                    name: 'user',
-                    redirect: '/user/self?showTab=like',
-                    children: [
-                        {
-                            path: 'self',
-                            component: () => import('../components/person.vue')
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            path: '/post',
-            name: 'post',
-            component: () => import('@/views/post.vue')
-        },
+const routes=[
+            {
+                path: '/',
+                component:Layout,
+                redirect: '/',
+                children: [
+                    {
+                        path: '/',
+                        name: 'recommend',
+                        component: () => import('../components/recommed.vue')
+                    },
 
-        {
-            path: '/:pathMatch(.*)*',
-            name: 'not-found',
-            component: () => import('@/views/not-found/not-found.vue')
-        }
-    ]
-})
+                    {
+                        path: '/user',
+                        name: 'user',
+                        redirect: '/user/self?showTab=like',
+                        children: [
+                            {
+                                path: 'self',
+                                component: () => import('../components/person.vue')
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                path: '/post',
+                name: 'post',
+                component: () => import('@/views/post.vue')
+            },
+
+            {
+                path: '/:pathMatch(.*)*',
+                name: 'not-found',
+                component: () => import('@/views/not-found/not-found.vue')
+            }
+]
 const router = createRouter({
     history: createWebHistory(),
     routes
 })
-
-VueRouter.beforeEach((to) => {
-    const modalId = to.query.modal_id
-    if (modalId) {
-        // 在这里根据 modalId 打开模态框
-        // 或者触发事件告知应用打开模态框
-    } else {
-        // 没有 modal_id 查询参数，正常进入路由
-    }
-})
+export default router
